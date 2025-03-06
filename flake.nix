36b5fe091d1f173feba2e72da050c5fba893b5f4
gusjengis <anthony.j.green@outlook.com>
2025-03-06 12:46:57 -0800

    moved some stuff to user space, added nightly firefox, now I can do dev

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inpts.nixpkgs.follotws = "nixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    firefox.url = "github:nix-community/flake-firefox-nightly";
+    firefox.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
 
   };
 
-  outputs = { self, nixpkgs, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
@@ -19,11 +21,14 @@
       nixosConfigurations = {
         nixos = lib.nixosSystem {
           inherit system;
+          specialArgs = {
+            inherit inputs;
+          };
           modules = [ ./configuration.nix ];
         };
       };
       homeConfigurations = {
-        gusjengis = home-manager.lim.homeManagerConfiguration {
+        gusjengis = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [ ./home.nix ];
         };
