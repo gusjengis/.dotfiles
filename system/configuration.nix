802d927f0161c95b8627a193d9aa394770506b54
gusjengis <anthony.j.green@outlook.com>
2025-03-26 20:39:14 -0700

    changed wallpaper, removed some uneccesary lines

--- system/configuration.nix
+++ system/configuration.nix
@@ -141,8 +141,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
     xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
     vulkan-tools
--- user/programs/file_explorer.nix
+++ user/programs/file_explorer.nix
@@ -4,7 +4,5 @@
   home.packages = with pkgs; [
     xfce.thunar
     xfce.tumbler
-    ffmpegthumbnailer
-    poppler
   ];
 }
