76cc48215799b985b0756558d41f02eb2ad289e3
gusjengis <anthony.j.green@outlook.com>
2025-04-01 13:24:58 -0700

    added lock screen program, hyprlock, still needs configuration

--- user/desktop_env/lock_screen.nix
+++ user/desktop_env/lock_screen.nix
@@ -2,4 +2,8 @@
 
 {
   home.packages = with pkgs; [ hyprlock ];
+
+  home.file.".config/hypr/hyprlock.conf".source =
+    ../config_files/hypr/hyprlock.conf;
+  home.file.".config/hypr/hyprlock.conf".force = true;
 }
